d67a19305ddd1e971c0ee1249b123bb2b9ac7fd0
Carvalho <kfcarvalho.018@gmail.com>
2019-08-15 23:51:45 -0300

    alteracao de classe

--- PFC1/src/controller/Register_Controller.java
+++ PFC1/src/controller/Register_Controller.java
@@ -1,16 +1,56 @@
+
 package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import com.jfoenix.controls.JFXHamburger;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TreeView;
+
 
 public class Register_Controller implements Initializable {
-        
+
+    @FXML
+    private TextField text_professor;
+
+    @FXML
+    private CheckBox cb_ativo;
+
+    @FXML
+    private CheckBox cb_inativo;
+
+    @FXML
+    private TreeView<?> treeview_professores;
+    
+    //##################################################
+
+    @FXML
+    private JFXHamburger hamb_menu;
+    
+   //#################################################
+
+    @FXML
+    private TextField text_cpf;
+
+    @FXML
+    private TextField text_nome;
+
+    @FXML
+    private TextField text_telefone;
+
+    @FXML
+    private TextField text_cidade;
+    
+    //################################################
+
+    @FXML
+    private Button bt_disciplina;
+
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
---
+++ PFC1/src/controller/Subject_reg_Controller.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import com.jfoenix.controls.JFXComboBox;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Subject_reg_Controller implements Initializable {
+
+    @FXML
+    private TextField text_disciplina;
+
+    @FXML
+    private Button bt_voltar;
+
+    @FXML
+    private JFXComboBox<?> cb_grau;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        
+        
+    }    
+    
+}
